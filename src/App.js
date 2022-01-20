a3fbf915338ae91d2f1bf4613b02f014cefe3af4
brainf0ck <thomas.shell.0by@gmail.com>
2022-01-20 22:57:48 +0500

    v1

--- src/App.js
+++ src/App.js
@@ -18,14 +18,14 @@ function App() {
         <Sidebar />
         <div className='content'> 
           <Routes>
-            <Route path="/" element={<KuraiContent/>} />
-            <Route path="/kurai_content" element={<KuraiContent/>} />
-            <Route path="/skot_content" element={<SkotContent />} />
-            <Route path="/muz_content" element={<MuzContent />} />
-            <Route path="/med_content" element={<MedContent />} />
-            <Route path="/lit_content" element={<LitContent />}/>
-            <Route path="/prazd_content" element={<PrazdContent />} />
-            <Route path="/teatr_content" element={<TeatrContent />} />
+            <Route path="/bashkiria/" element={<KuraiContent/>} />
+            <Route path="/bashkiria/kurai_content" element={<KuraiContent/>} />
+            <Route path="/bashkiria/skot_content" element={<SkotContent />} />
+            <Route path="/bashkiria/muz_content" element={<MuzContent />} />
+            <Route path="/bashkiria/med_content" element={<MedContent />} />
+            <Route path="/bashkiria/lit_content" element={<LitContent />}/>
+            <Route path="/bashkiria/prazd_content" element={<PrazdContent />} />
+            <Route path="/bashkiria/teatr_content" element={<TeatrContent />} />
           </Routes>
         </div>
       </div>
--- src/components/Sidebar/Items/Items.jsx
+++ src/components/Sidebar/Items/Items.jsx
@@ -5,13 +5,13 @@ import { Link } from "react-router-dom";
 const Items = () => {
     return (
         <div className='items'>
-            <Link to="/kurai_content"><p className="item">Башкортостан</p></Link>
-            <Link to="/skot_content"><p className="item">Скотоводство</p></Link>
-            <Link to="/muz_content"><p className="item">Музыка</p></Link>
-            <Link to="/med_content"><p className="item">Башкирский мёд</p></Link>
-            <Link to="/lit_content"><p className="item">Литература</p></Link>
-            <Link to="/prazd_content"><p className="item">Праздники</p></Link>
-            <Link to="/teatr_content"><p className="item">Театры</p></Link>
+            <Link to="/bashkiria/kurai_content"><p className="item">Башкортостан</p></Link>
+            <Link to="/bashkiria/skot_content"><p className="item">Скотоводство</p></Link>
+            <Link to="/bashkiria/muz_content"><p className="item">Музыка</p></Link>
+            <Link to="/bashkiria/med_content"><p className="item">Башкирский мёд</p></Link>
+            <Link to="/bashkiria/lit_content"><p className="item">Литература</p></Link>
+            <Link to="/bashkiria/prazd_content"><p className="item">Праздники</p></Link>
+            <Link to="/bashkiria/teatr_content"><p className="item">Театры</p></Link>
         </div>
     );
 };
